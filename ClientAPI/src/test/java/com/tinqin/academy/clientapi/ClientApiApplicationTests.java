2d48adb8b64042e1f15b9aa41ec4ee0f334c0ea3
Nikola Slavchev <nslavchev48@gmail.com>
2022-08-03 12:27:35 +0300

    Init project

---
+++ ClientAPI/src/test/java/com/tinqin/academy/clientapi/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
